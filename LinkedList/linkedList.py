ed4c86f9ef8beba71aba5e383cb11a0a701f5f7b
Parth Ekbote <paraedox0@gmail.com>
2025-02-18 09:03:04 +0000

    Created Linked List traverse function

--- LinkedList/linkedList.py
+++ LinkedList/linkedList.py
@@ -12,7 +12,15 @@ class LinkedList:
         new_node.next = self.head
         self.head = new_node
 
-    
+    def traverse(self):
+        if self.head is None:
+            print("Null")
+            return
+        current = self.head
+        while current is not None:
+            print(f"{current.data} -->", end = " ")
+            current = current.next
+        print("Null")
 
 list1 = LinkedList()
 list1.insert_at_end(10)
