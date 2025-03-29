f1a895e0bade2ade049b7644cd4fd34ee2bf48ea
Parth Ekbote <paraedox0@gmail.com>
2025-03-29 09:50:27 +0000

    Hash table methods

--- Hash_Table/hashTable.py
+++ Hash_Table/hashTable.py
@@ -1,17 +1,24 @@
-# Using dict (Hash Map)
-hash_map = {}
+# # Using dict (Hash Map)
+# hash_map = {}
 
-# Insertion
-hash_map["cat"] = 10
-hash_map["dog"] = 20
-hash_map["tac"] = 30  # Different key, even if it might hash similarly
-hash_map["bird"] = 40
-print("Hash Map after insertions:", hash_map)
+# # Insertion
+# hash_map["cat"] = 10
+# hash_map["dog"] = 20
+# hash_map["tac"] = 30  # Different key, even if it might hash similarly
+# hash_map["bird"] = 40
+# print("Hash Map after insertions:", hash_map)
 
-# Lookup
-print("Value for 'cat':", hash_map.get("cat"))  # 10
-print("Value for 'fish':", hash_map.get("fish"))  # None (not found)
+# # Lookup
+# print("Value for 'cat':", hash_map.get("cat"))  # 10
+# print("Value for 'fish':", hash_map.get("fish"))  # None (not found)
 
-# Deletion
-del hash_map["cat"]
-print("Hash Map after deleting 'cat':", hash_map)
+# # Deletion
+# del hash_map["cat"]
+# print("Hash Map after deleting 'cat':", hash_map)
+
+
+list = [1, 2, 1, 5, 7, 9, 1, 2]
+
+my_set = set(list)
+print(my_set)
+my_dict = {}
