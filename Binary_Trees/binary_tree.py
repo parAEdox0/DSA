a4af144ebf48b142a6903c3918f9e9c70360733b
Parth Ekbote <paraedox0@gmail.com>
2025-04-03 14:12:18 +0000

    Implementing Binary tree

---
+++ Binary_Trees/binary_tree.py
@@ -0,0 +1,13 @@
+class TreeNode:
+    def __init__(self, val, left = None, right = None):
+        self.val = val
+        self.left = left
+        self.right = right
+    
+    def __str__(self):
+        return f"{self.val}"
+    
+
+    
+
+
